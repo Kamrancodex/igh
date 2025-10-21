3f7f7305104064cc0de88b2086194b3e7a260909
Kamran <155973381+Kamrancodex@users.noreply.github.com>
2025-10-21 13:31:54 +0530

    Update brand names - Replace with Danny Boys, Marigold, Tenk West Bank, Bellavie, Redspace, The JNG Grill, Hog Heaven, and Fa-ray's

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -18,13 +18,13 @@ async function seed() {
     // Seed businesses
     const businesses = [
       {
-        name: "Majestic Hall",
+        name: "Danny Boys",
         image: "/business-1.webp",
-        description: "Where Dreams Come True",
-        link: "https://majestic-rdj.media",
+        description: "Authentic dining experience with exceptional service",
+        link: "#",
         socials: {
-          instagram: "https://www.instagram.com/Majestichallcle",
-          facebook: "https://www.facebook.com/154478397755479",
+          instagram: "#",
+          facebook: "#",
           twitter: "#",
           website: "#",
         },
@@ -43,24 +43,76 @@ async function seed() {
         },
       },
       {
-        name: "Danny Boy's",
+        name: "Tenk West Bank",
         image:
           "https://images.unsplash.com/photo-1470337458703-46ad1756a187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by-wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
         description: "Sophisticated cocktail bar & lounge",
-        link: "https://www.dannyboyspizza.com/",
+        link: "#",
         socials: {
-          instagram: "https://www.instagram.com/dannyboyspizza1991",
-          facebook: "https://www.facebook.com/dannyboyspizza1991",
+          instagram: "#",
+          facebook: "#",
           twitter: "#",
           website: "#",
         },
       },
       {
-        name: "Harvest Table",
+        name: "Bellavie",
         image:
           "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by-wYWdlfHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-        description: "Farm-to-table dining experience",
-        link: "/businesses/harvest",
+        description: "Elegant dining and entertainment venue",
+        link: "#",
+        socials: {
+          instagram: "#",
+          facebook: "#",
+          twitter: "#",
+          website: "#",
+        },
+      },
+      {
+        name: "Redspace",
+        image:
+          "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+        description: "Modern dining and entertainment space",
+        link: "#",
+        socials: {
+          instagram: "#",
+          facebook: "#",
+          twitter: "#",
+          website: "#",
+        },
+      },
+      {
+        name: "The JNG Grill",
+        image:
+          "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+        description: "Premium grilled cuisine and fine dining",
+        link: "#",
+        socials: {
+          instagram: "#",
+          facebook: "#",
+          twitter: "#",
+          website: "#",
+        },
+      },
+      {
+        name: "Hog Heaven",
+        image:
+          "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+        description: "Authentic barbecue and comfort food",
+        link: "#",
+        socials: {
+          instagram: "#",
+          facebook: "#",
+          twitter: "#",
+          website: "#",
+        },
+      },
+      {
+        name: "Fa-ray's",
+        image:
+          "https://images.unsplash.com/photo-1551218808-94e220e084d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+        description: "Unique culinary experience and hospitality",
+        link: "#",
         socials: {
           instagram: "#",
           facebook: "#",
